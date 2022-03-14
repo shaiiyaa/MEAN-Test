20ff307b2395f504d923d6c03927850297fbd18a
shaiiyaa <shaiiyaakobov@gmail.com>
2022-03-14 16:46:05 +0200

    deploy

---
+++ data-access-layer/orders-dal.js
@@ -0,0 +1,24 @@
+const OrdersModel = require('../models/orders-model');
+const CartModel = require('../models/cart-model');
+
+async function getOccupied() {
+    const now = new Date();
+    const correct_timezone = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+    const orders = await OrdersModel.find({delivery_date: {$gte: correct_timezone}}).exec();
+    const dates = orders.map(d => {return d.delivery_date});
+    return dates;
+};
+
+async function addOrder(newOrder) {
+    const order = await newOrder.save();
+    await CartModel.findByIdAndUpdate({_id: newOrder.cartID}, {closed: true});
+    return order;
+};
+
+
+
+module.exports = {
+    getOccupied,
+    addOrder 
+};
